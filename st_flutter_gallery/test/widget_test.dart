97eba9f0b6b209c3390271ddf29065a5ca3b265c
Soto.Poul <p709723778@126.com>
2021-12-17 14:33:14 +0800

    feat: 修复警告⚠️

--- st_flutter_gallery/test/widget_test.dart
+++ st_flutter_gallery/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:st_flutter_gallery/main.dart';
+import 'package:st/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
