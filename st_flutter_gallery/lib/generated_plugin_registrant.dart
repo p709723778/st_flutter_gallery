bb15b7dcf85b6715166747444993bf271e10eaa4
Soto.Poul <p709723778@126.com>
2022-06-17 22:40:18 +0800

    feat: 更新适配flutter 3.1.0

--- st_flutter_gallery/lib/generated_plugin_registrant.dart
+++ st_flutter_gallery/lib/generated_plugin_registrant.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:device_info_plus_web/device_info_plus_web.dart';
@@ -14,7 +15,7 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
+void registerPlugins(final Registrar registrar) {
   ConnectivityPlusPlugin.registerWith(registrar);
   DeviceInfoPlusPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
